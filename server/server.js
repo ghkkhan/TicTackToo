c7bfded507020e67b9847512ff6f414acfa27b6a
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-01-09 15:23:07 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,14 +25,35 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 app.use(express.json());
 
+function generate_token(length){
+    //edit the token allowed characters
+    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+    var b = [];  
+    for (var i=0; i<length; i++) {
+        var j = (Math.random() * (a.length-1)).toFixed(0);
+        b[i] = a[j];
+    }
+    return b.join("");
+}
 
-app.post('/create_game', (req, res) => {
+roomCount = 0
+
+app.post('/create', (req, res) => {
     console.log(req.body)
-    res.send('Hello');
+	users.push(req.body.username)
+	room_and_code.push({
+		users: [],
+		room_code: hex_md5(roomCount)
+	})
+	data = {
+		roomCode: hex_md5(roomCount)
+	}
+	roomCount++
+    res.send(data);
 })
 
 app.listen(port, () => console.log(`example app listing to ${port}`));
 
-var rooms = [];
+var room_and_code = []; // consists of {users: [], room_code: aaa}
 var users = [];
 var codes = [];
