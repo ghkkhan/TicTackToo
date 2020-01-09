835ae2efed4695facde7170adc55b4fb457f56e3
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-01-09 14:19:32 -0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,35 @@
+// imports
 const express = require('express');
 const path = require('path');
+const bodyParser = require("body-parser");
 
+// setting up express
 const app = express();
 const port = 3000;
 
+// send index on main page
 let index = path.join(__dirname, '/../index.html');
 app.use(express.static('../Frontend/mainpage'));
-
 app.get('/', (req, res) => {
     res.sendFile(index);
 });
+
+// https://stackoverflow.com/questions/56624471/express-req-body-is-always-empty
+// used for getting POST body
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
+app.use(express.json());
+
+
+app.post('/create_game', (req, res) => {
+    console.log(req.body)
+    res.send('Hello');
+})
 
 app.listen(port, () => console.log(`example app listing to ${port}`));
 
 var rooms = [];
 var users = [];
 var codes = [];
-
-app.post('/test', (req, res) => {
-    console.log("printedtest stuff");
-    res.send('Hello');
-})
